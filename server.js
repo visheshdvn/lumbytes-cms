4ee2d42a8508a356024d8098f6f68e0143430679
visheshdvn <vishesh.dh1@gmail.com>
2021-10-10 13:35:00 +0530

    necessary files/folders created

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require("express");
-const { PrismaClient } = require("@prisma/client");
+const path = require("path");
 const cors = require("cors");
-
+require("dotenv").config();
 const app = express();
 
 // init middleware
-app.use(express.json({ extended: false }));
+app.use(express.json({ extended: true }));
 app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
+// client
+const clientPath = path.join(__dirname, "client")
+app.use(express.static(clientPath));
 
 // define routes
-app.use("/api/blogposts", require("./routes/api/blogposts"))
+app.get("/", (req, res, next) => {
+  // console.log(res);
+  // next();
+  // res.status(200).send("<h1>Hello!</h1>")
+  res.sendFile(path.join(clientPath, ".next", "server", "pages", "index.html"))
+});
+
+app.use("/api/blogposts", require("./routes/api/blogposts"));
+
+app.use((req, res) => {
+  res.status(404).json({ status: "404 - not found" });
+});
 
 const PORT = process.env.PORT || 2000;
 
