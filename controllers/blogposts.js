f11f375cf965e0d08f57bccc2afa36bc0b59572e
visheshdvn <vishesh.dh1@gmail.com>
2021-10-16 17:53:06 +0530

    validators for create and update post created

--- controllers/blogposts.js
+++ controllers/blogposts.js
@@ -5,7 +5,7 @@ import { Router } from "express";
 import { PrismaClient } from "@prisma/client";
 import { validationResult } from "express-validator";
 // validator imports
-import { createBlogpost } from "../validators/blogposts";
+import { createBlogpost, updateBlogpost } from "../validators/blogposts";
 
 const router = Router();
 const { blogposts, testdb } = new PrismaClient();
@@ -50,7 +50,6 @@ router.post("/create", createBlogpost, async (req, res) => {
     topPick,
     date,
   } = req.body;
-  console.log("body - ", req.body);
   try {
     const blog = await blogposts.create({
       data: {
@@ -71,13 +70,44 @@ router.post("/create", createBlogpost, async (req, res) => {
       },
     });
 
-    console.log(blog);
-
     res.status(200).json({ blogpost: blog, status: "created" });
   } catch (err) {
     console.log(err);
     res.status(500).json({ status: "Error - not created" });
   }
+});
+
+// @route   POST api/blogposts/create
+// @desc    create a blogpost
+// @access  Public
+router.patch("/:postId", updateBlogpost, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  // console.log(req.body);
+  const { postId } = req.params;
+  const {
+    title,
+    slug,
+    metaDescription,
+    excerpt,
+    content,
+    banner,
+    banneralt,
+    minuteRead,
+    featured,
+    topPick,
+    date,
+  } = req.body;
+
+  const blogpost = await blogposts.findMany({
+    where: {
+      id: postId,
+    },
+  });
+  console.log(blogpost);
+  res.status(200).send("got here");
 });
 
 export default router;
--- validators/blogposts.js
+++ validators/blogposts.js
@@ -3,27 +3,97 @@ import { PrismaClient } from "@prisma/client";
 
 const { blogposts } = new PrismaClient();
 
-const createBlogpost = [
-  check("title", "title is required").not().isEmpty(),
-  check("title", "title should be a string").isString(),
-  check("slug", "slug should be a valid string")
+// DRY checks
+const commonInCreateAndUpdate = [
+  check("metaDescription", "meta descrption should be a valid string")
+    .optional()
     .isString()
-    .custom((slug) => {
-      if (slug.trim() === "") {
-        return Promise.reject("String cannot be empty/only-spaces ");
-      } else {
-        return Promise.resolve("String is valid.");
-      }
-    }),
-  check("slug", "slug should be unique").custom((slug) => {
-    return blogposts.findMany({ where: { slug } }).then((blogpost) => {
-      if (blogpost.length > 0) {
-        return Promise.reject("slug already taken");
-      } else {
-        return Promise.resolve("slug allowed");
-      }
-    });
-  }),
+    .custom((value) => notEmpty(value)),
+  check("metaDescription", "meta descrption should be under 201 characters")
+    .optional()
+    .isLength({ max: 200 }),
+  check("excerpt", "excerpt should be a valid string")
+    .optional()
+    .isString()
+    .custom((value) => notEmpty(value)),
+  check("excerpt", "excerpt should be under 150 characters")
+    .optional()
+    .isLength({ max: 150 }),
+  check("minuteRead", "minute read should be an Integer")
+    .optional()
+    .not()
+    .isString(),
+  check("minuteRead", "minute read should be greater tham 0")
+    .optional()
+    .isInt({ min: 1 }),
+  check("topPick", "topPick should be boolean")
+    .optional()
+    .not()
+    .isString()
+    .not()
+    .isFloat()
+    .isBoolean(),
+  check("featured", "featured should be boolean")
+    .optional()
+    .not()
+    .isString()
+    .not()
+    .isFloat()
+    .isBoolean(),
+  check("date", "date should be iso string")
+    .optional()
+    .isISO8601()
+    .custom((date) => /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(date)),
 ];
 
-export { createBlogpost };
+const notEmpty = (value) => {
+  if (value.trim() === "") {
+    return Promise.reject("value cannot be empty or only spaces ");
+  } else {
+    return Promise.resolve("value is valid.");
+  }
+};
+
+// creating a blogpost
+const createBlogpost = [
+  check("title", "title should be a non empty string")
+    .isString()
+    .custom((title) => notEmpty(title)),
+  check("slug", "slug should be a valid string")
+    .isString()
+    .custom((slug) => notEmpty(slug))
+    .custom((slug) => {
+      return blogposts.findMany({ where: { slug } }).then((blogpost) => {
+        if (blogpost.length > 0) {
+          return Promise.reject("slug should be unique");
+        } else {
+          return Promise.resolve("slug valid");
+        }
+      });
+    }),
+  ...commonInCreateAndUpdate,
+];
+
+// update blogpost validation
+const updateBlogpost = [
+  check("title", "title should be a string")
+    .optional()
+    .isString()
+    .custom((title) => notEmpty(title)),
+  check("slug", "slug should be a valid string")
+    .optional()
+    .isString()
+    .custom((slug) => notEmpty(slug))
+    .custom((slug) => {
+      return blogposts.findMany({ where: { slug } }).then((blogpost) => {
+        if (blogpost.length > 0) {
+          return Promise.reject("slug should be unique");
+        } else {
+          return Promise.resolve("slug valid");
+        }
+      });
+    }),
+  ...commonInCreateAndUpdate,
+];
+
+export { createBlogpost, updateBlogpost };
