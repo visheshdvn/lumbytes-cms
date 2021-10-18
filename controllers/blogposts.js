806b43777000af5f433cb2e08e7e955709e843a4
visheshdvn <vishesh.dh1@gmail.com>
2021-10-19 03:54:43 +0530

    tags controllers created

--- controllers/blogposts.js
+++ controllers/blogposts.js
@@ -14,13 +14,16 @@ const { blogposts, testdb } = new PrismaClient();
 // @desc    get all published blogposts
 // @access  Public
 router.get("/", async (req, res) => {
+  const { skip, take } = req.query;
   try {
     const blog = await blogposts.findMany({
       where: {
         published: false,
       },
+      skip: +skip || undefined,
+      take: +take || undefined,
     });
-    
+
     res.status("200").json(blog);
   } catch (err) {
     console.log(err);
@@ -87,6 +90,11 @@ router.post("/create", createBlogpost, async (req, res) => {
         created_at: new Date().toISOString(),
         upadted_at: new Date().toISOString(),
         published: false,
+        author: {
+          connect: {
+            id: "91296147-dc68-43e0-968a-55c118c2a3ef",
+          },
+        },
       },
     });
 
@@ -97,10 +105,10 @@ router.post("/create", createBlogpost, async (req, res) => {
   }
 });
 
-// @route   POST api/blogposts/create
-// @desc    create a blogpost
+// @route   POST api/blogposts/:postId
+// @desc    update a blogpost
 // @access  Public
-router.patch("/:postId", updateBlogpost, async (req, res) => {
+router.patch("/update/:postId", updateBlogpost, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
@@ -120,39 +128,91 @@ router.patch("/:postId", updateBlogpost, async (req, res) => {
     featured,
     topPick,
     date,
+    authorId,
   } = req.body;
+  console.log(req.body);
 
   try {
-    const curr_blogpost = await blogposts.findUnique({
-      where: {
-        id: postId,
-      },
-    });
+    // const curr_blogpost = await blogposts.findUnique({
+    //   select: {
+    //     title: true,
+    //     slug: true,
+    //     metaDescription: true,
+    //     excerpt: true,
+    //     content: true,
+    //     banner: true,
+    //     banneralt: true,
+    //     minuteRead: true,
+    //     featured: true,
+    //     topPick: true,
+    //     date: true,
+    //     authorId: true,
+    //   },
+    //   where: {
+    //     id: postId,
+    //   },
+    // });
 
+    // console.log("current post", curr_blogpost);
     const updated_post = await blogposts.update({
       where: {
         id: postId,
       },
       data: {
-        title: title === undefined ? curr_blogpost.title : title,
-        slug: slug === undefined ? curr_blogpost.slug : slug,
-        excerpt: excerpt === undefined ? curr_blogpost.excerpt : excerpt,
-        content: content === undefined ? curr_blogpost.content : content,
-        featured: featured === undefined ? curr_blogpost.featured : featured,
-        topPick: topPick === undefined ? curr_blogpost.topPick : topPick,
-        date: date === undefined ? curr_blogpost.date : date,
-        banner: banner === undefined ? curr_blogpost.banner : banner,
-        banneralt:
-          banneralt === undefined ? curr_blogpost.banneralt : banneralt,
-        metaDescription:
-          metaDescription === undefined
-            ? curr_blogpost.metaDescription
-            : metaDescription,
-        minuteRead:
-          minuteRead === undefined ? curr_blogpost.minuteRead : minuteRead,
+        // title: title === undefined ? curr_blogpost.title : title,
+        // slug: slug === undefined ? curr_blogpost.slug : slug,
+        // excerpt: excerpt === undefined ? curr_blogpost.excerpt : excerpt,
+        // content: content === undefined ? curr_blogpost.content : content,
+        // featured: featured === undefined ? curr_blogpost.featured : featured,
+        // topPick: topPick === undefined ? curr_blogpost.topPick : topPick,
+        // date: date === undefined ? curr_blogpost.date : date,
+        // banner: banner === undefined ? curr_blogpost.banner : banner,
+        // banneralt:
+        //   banneralt === undefined ? curr_blogpost.banneralt : banneralt,
+        // metaDescription:
+        //   metaDescription === undefined
+        //     ? curr_blogpost.metaDescription
+        //     : metaDescription,
+        // minuteRead:
+        //   minuteRead === undefined ? curr_blogpost.minuteRead : minuteRead,
+        // upadted_at: new Date().toISOString(),
+        // authorId: authorId === undefined ? curr_blogpost.authorId : authorId,
+        ...req.body,
       },
     });
     res.status(200).json({ blogpost: updated_post, msg: "updated" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ blogpost: null, msg: "Server Error" });
+  }
+});
+
+// @route   POST api/blogposts/publish/:postId
+// @desc    update a blogpost
+// @access  Public
+router.patch("/publish/:postId", async (req, res) => {
+  const { postId } = req.params;
+
+  try {
+    const blogpost = await blogposts.findUnique({
+      select: {
+        title: true,
+        slug: true,
+        metaDescription: true,
+        excerpt: true,
+        content: true,
+        banner: true,
+        banneralt: true,
+        minuteRead: true,
+        date: true,
+        author,
+      },
+      where: {
+        id: postId,
+      },
+    });
+    console.log(blogpost);
+    res.status(200).send("💯");
   } catch (err) {
     console.log(err);
     res.status(500).json({ blogpost: null, msg: "Server Error" });
--- controllers/tags.js
+++ controllers/tags.js
@@ -1,29 +1,89 @@
 import { Router } from "express";
 import { PrismaClient } from "@prisma/client";
-import { check, validationResult } from "express-validator";
-
+import { validationResult } from "express-validator";
+// tags imports
+import { createTag } from "../validators/tags";
 
 const router = Router();
-const { tags, testdb } = new PrismaClient();
+const { tags } = new PrismaClient();
 
 // @route   GET api/tags
 // @desc    get all published tags
 // @access  Public
 router.get("/", async (req, res) => {
+  const { skip, take } = req.query;
   try {
     const tag = await tags.findMany({
       where: {
-        published: false
-      }
+        published: true,
+      },
+      select: {
+        id: true,
+        tagname: true,
+        color: true,
+        metaDescription: true,
+        ogimg: true,
+        ogalt: true,
+      },
+      skip: +skip || undefined,
+      take: +take || undefined,
     });
-    console.log(tag);
     res.status("200").json(tag);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+// @route   GET api/tags/:id
+// @desc    find a tag from the id
+// @access  Public
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const tag = await tags.findMany({
+      where: {
+        published: true,
+        id: +id,
+      },
+    });
+    res.status("200").json(tag[0]);
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: "not found any" });
   }
 });
 
+// @route   POST api/tags/create
+// @desc    create a blogpost
+// @access  Public
+router.post("/create", createTag, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
+  const { tagname, color, metaDescription, ogimg, ogalt } = req.body;
 
-export default router
+  try {
+    const tag = await tags.create({
+      data: {
+        tagname,
+        color,
+        metaDescription,
+        ogimg,
+        ogalt,
+        created_at: new Date().toISOString(),
+        upadted_at: new Date().toISOString(),
+        published: false,
+      },
+    });
+
+    res.status(200).json({ tag, status: "created" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ status: "Error - not created" });
+  }
+});
+
+export default router;
